263a7f99645bb5141f0a312a8106d53d9691d2e0
Chinedu Arinze <c.s.arinze@gmail.com>
2019-08-08 01:17:54 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,31 @@
+class PigLatinizer
+
+  attr_accessor :string
+
+  def initialize(string)
+    @string = string
+  end
+
+  def phrase
+    words = string.split(" ")
+    phrase = words.map {|word| piglatinize_word(word)}
+    phrase.join(" ")
+  end
+
+  def word(word)
+    first_letter = word[0].downcase
+    if ["a", "e", "i", "o", "u"].include?(first_letter)
+        "#{word}way"
+    else
+      consonants = []
+      consonants << word[0]
+        if ["a", "e", "i", "o", "u"].include?(word[1]) == false
+          consonants << word[1]
+          if ["a", "e", "i", "o", "u"].include?(word[2]) == false
+            consonants << word[2]
+          end
+        end
+      "#{word[consonants.length..-1] + consonants.join + "ay"}"
+    end
+  end
+end
